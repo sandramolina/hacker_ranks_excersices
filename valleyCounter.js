c737392ba7660c71e9bcf189b99707f6ad0810c9
Sandra Molina Palencia <smolinapalencia@gmail.com>
2023-03-29 08:36:12 +0100

    Adds logic to get the array containing the values of the path

--- valleyCounter.js
+++ valleyCounter.js
@@ -9,15 +9,32 @@ string2 = 'UDDDUDUU'; //Returns 1
 //Transform string into an array of numbers
 const array = string2.split('');
 arrayOfValues = array.map((step) => (step === 'U' ? 1 : -1));
+// console.log(arrayOfValues);
+
+//A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
+// Valley --> when the running score is over 0 and then becomes <0 until it becomes > 0.
 
 // Calculate path running total and save it into an array
 //TODO: the array must save the running value and not just the sum of the prior value plus the current one
-pathRunningValue = arrayOfValues.map((value, i) => {
-  if (i === 0) {
-    return value;
-  } else {
-    console.log(i, value, arrayOfValues[i - 1], value + arrayOfValues[i - 1]);
-    //return value + arrayOfValues[i - 1];
-  }
-});
+const pathRunningValueArray = (arr) => {
+  let runningTotal = 0;
+  let runningTotalArray = [];
+  arr.forEach((element) => {
+    runningTotalArray.push((runningTotal += element));
+    //console.log('element: ', element, 'runningT: ', runningTotal);
+  });
+  return runningTotalArray;
+};
+console.log(pathRunningValueArray(arrayOfValues));
+
+//TODO:Given an array of values, account for the number of times the running value is <0 right before it's 0 again.
+
+// pathRunningValue = arrayOfValues.map((value, i) => {
+//   if (i === 0) {
+//     return value;
+//   } else {
+//     console.log(i, value, arrayOfValues[i - 1], value + arrayOfValues[i - 1]);
+//     //return value + arrayOfValues[i - 1];
+//   }
+// });
 //console.log(pathRunningValue);
